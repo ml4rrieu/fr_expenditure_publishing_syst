4e9505955beb8214990d12ad4e959e93b39cd2b8
Maxence Larrieu <m@larri.eu>
2022-01-10 16:55:11 +0100

    update

--- collapse_acquisition_and_apc.py
+++ collapse_acquisition_and_apc.py
@@ -35,6 +35,8 @@ ax.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('€{x:,.0f}'))
 ax.bar(reading_price.keys(), reading_price.values(), label = "to read (online scientific resources)" )
 ax.bar(apc_price.keys(), apc_price.values(), bottom = list(reading_price.values()), label = "to publish (APC)" )
 
+
+
 #remove origin
 yticks = ax.yaxis.get_major_ticks()
 yticks[0].label1.set_visible(False)
@@ -42,13 +44,16 @@ yticks[0].label1.set_visible(False)
 #set grid
 ax.grid(color='grey', linestyle='--', linewidth=0.5)
 plt.gca().xaxis.grid(False)
+plt.gca().set_ylim([0, 130000000]) # set ymax
+
+# display and change legend order
+handles, labels = ax.get_legend_handles_labels()
+order = [1, 0]
+ax.legend([handles[idx] for idx in order],[labels[idx] for idx in order], 
+    bbox_to_anchor=(0.5, 0.7), fontsize = 15, loc="upper center", borderaxespad =1.7)
 
 
-#legend
-plt.legend(loc="upper center", bbox_to_anchor=(0.5, 0.7), fontsize = 15)
 plt.title("French spending on scientific publishing", fontsize = 24, y = 0.99)
-
-
 plt.savefig("french_spending.png")
 
 #print(len(df2015))
